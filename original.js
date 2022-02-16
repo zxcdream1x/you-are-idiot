36d173166ad283522819cae2cd158401792fefc4
7vqro <62899442+7vqro@users.noreply.github.com>
2022-02-16 09:42:46 +0300

    Create original.js

---
+++ original.js
@@ -0,0 +1,78 @@
+var xOff = 5;
+var yOff = 5;
+var xPos = 400;
+var yPos = -100;
+var flagRun = 1;
+
+function changeTitle(title) {
+	document.title = title;
+}
+
+function openWindow(url) {
+	aWindow = window.open(url, "_blank", 'menubar=no, status=no, toolbar=no, resizable=no, width=357, height=330, titlebar=no, alwaysRaised=yes');
+}
+
+function proCreate() {	
+	for (var i = 0; i < 5; i++) {
+		openWindow('you.html');
+	}
+}
+
+function newXlt() {
+	xOff = Math.ceil(-6 * Math.random()) * 5 - 10;
+	window.focus();
+}
+
+function newXrt() {
+	xOff = Math.ceil(7 * Math.random())  * 5 - 10;
+	window.focus();
+}
+
+function newYup() {
+	yOff = Math.ceil(-6 * Math.random()) * 5 - 10;
+	window.focus();
+}
+
+function newYdn() {
+	yOff = Math.ceil( 7 * Math.random()) * 5 - 10;
+	window.focus();
+}
+
+function fOff(){
+	flagRun = 0;
+}
+
+function playBall() {
+    xPos += xOff;
+    yPos += yOff;
+    
+	if (xPos > screen.width - 357) newXlt();    
+	if (xPos < 0) newXrt();
+    
+	if (yPos > screen.height - 330) newYup(); 		
+	if (yPos < 0) newYdn();
+    
+	if (flagRun == 1) {
+        window.moveTo(xPos, yPos);
+        setTimeout('playBall()', 1);
+    }
+}
+
+window.onload = function () {
+	flagRun = 1;
+	
+	playBall();
+	
+	return true;
+}
+
+window.onmouseout = function () {
+	proCreate();
+
+	return null;
+};
+
+window.oncontextmenu = function() {
+	
+	return false;
+}
